bc0f9a4cf581b46ae3ac065e41708f3f2a66e808
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-01-31 16:13:25 +0100

    Update main.tf

--- accounts/test-vpc-peering/main.tf
+++ accounts/test-vpc-peering/main.tf
@@ -129,7 +129,9 @@ module "confluent_kafka_topics" {
     confluent_api_key.app-manager-kafka-api-key,
     confluent_network.peering,
     confluent_peering.aws,
-    confluent_role_binding.app-manager-kafka-cluster-admin
+    confluent_role_binding.app-manager-kafka-cluster-admin,
+    aws_vpc_peering_connection_accepter.peer,
+    aws_route.r  
   ]
   
 }
