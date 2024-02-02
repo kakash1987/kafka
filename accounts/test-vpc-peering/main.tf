240885b942ec328a8cbbac66c73a98200a9957dc
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-02 17:10:14 +0100

    Update main.tf

--- accounts/test-vpc-peering/main.tf
+++ accounts/test-vpc-peering/main.tf
@@ -3,13 +3,13 @@ resource "confluent_environment" "staging" {
 }
 
 
-output "confluent_environment_id" {
-  value = confluent_environment.staging.id
-}
+#output "confluent_environment_id" {
+#  value = confluent_environment.staging.id
+#}
 
-output "confluent_kafka_cluster_id" {
-  value = confluent_kafka_cluster.dedicated.id
-}
+#output "confluent_kafka_cluster_id" {
+#  value = confluent_kafka_cluster.dedicated.id
+#}
 
 // 'app-manager' service account is required in this configuration to create 'orders' topic and grant ACLs
 // to 'app-producer' and 'app-consumer' service accounts.
@@ -18,14 +18,14 @@ resource "confluent_service_account" "app-manager" {
   description  = "Service account to manage 'inventory' Kafka cluster"
 }
 
-#resource "confluent_service_account" "app-manager2" {
-#  display_name = "app-manager2"
-#  description  = "Service account to manage 'inventory2' Kafka cluster"
-#}
-
-output "confluent_service_account_app-manager" {
-  value = confluent_service_account.app-manager.id
+resource "confluent_service_account" "app-manager2" {
+  display_name = "app-manager2"
+  description  = "Service account to manage 'inventory2' Kafka cluster"
 }
+
+#output "confluent_service_account_app-manager" {
+#  value = confluent_service_account.app-manager.id
+#}
 
 resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
   principal   = "User:${confluent_service_account.app-manager.id}"
@@ -37,11 +37,15 @@ depends_on = [
   ]
 }
 
-#resource "confluent_role_binding" "app-manager-kafka-cluster-admin2" {
-#  principal   = "User:${confluent_service_account.app-manager2.id}"
-#  role_name   = "CloudClusterAdmin"
-#  crn_pattern = confluent_kafka_cluster.dedicated2.rbac_crn
-#}
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin2" {
+  principal   = "User:${confluent_service_account.app-manager2.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.dedicated2.rbac_crn
+depends_on = [
+    confluent_kafka_cluster.dedicated2,
+    confluent_service_account.app-manager2
+  ]
+}
 
 resource "confluent_api_key" "app-manager-kafka-api-key" {
   display_name = "app-manager-kafka-api-key"
@@ -69,25 +73,31 @@ resource "confluent_api_key" "app-manager-kafka-api-key" {
   ]
 }
 
-#resource "confluent_api_key" "app-manager-kafka-api-key2" {
-#  display_name = "app-manager-kafka-api-key2"
-#  description  = "Kafka API Key that is owned by 'app-manager2' service account"
-#  disable_wait_for_ready = "true"
-#  owner {
-#    id          = confluent_service_account.app-manager2.id
-#    api_version = confluent_service_account.app-manager2.api_version
-#    kind        = confluent_service_account.app-manager2.kind
-#  }
+resource "confluent_api_key" "app-manager-kafka-api-key2" {
+  display_name = "app-manager-kafka-api-key2"
+  description  = "Kafka API Key that is owned by 'app-manager2' service account"
+  disable_wait_for_ready = "true"
+  owner {
+    id          = confluent_service_account.app-manager2.id
+    api_version = confluent_service_account.app-manager2.api_version
+    kind        = confluent_service_account.app-manager2.kind
+  }
 
-#  managed_resource {
-#    id          = confluent_kafka_cluster.dedicated2.id
-#    api_version = confluent_kafka_cluster.dedicated2.api_version
-#    kind        = confluent_kafka_cluster.dedicated2.kind
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated2.id
+    api_version = confluent_kafka_cluster.dedicated2.api_version
+    kind        = confluent_kafka_cluster.dedicated2.kind
 
-#    environment {
-#      id = confluent_environment.staging.id
-#    }
-#  }
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+depends_on = [
+    confluent_kafka_cluster.dedicated2,
+    confluent_environment.staging,
+    confluent_service_account.app-manager2  
+  ]
+}
 
   # The goal is to ensure that confluent_role_binding.app-manager-kafka-cluster-admin is created before
   # confluent_api_key.app-manager-kafka-api-key is used to create instances of
@@ -136,32 +146,40 @@ module "confluent_kafka_topics" {
   
 }
 
-#module "confluent_kafka_topics2" {
-#  source = "../../modules/confluent_kafka_topics_module"
+module "confluent_kafka_topics2" {
+  source = "../../modules/confluent_kafka_topics_module"
 
   # kafka_id            = var.kafka_id
   # kafka_rest_endpoint = var.kafka_rest_endpoint
   # kafka_api_key       = var.kafka_api_key
   # kafka_api_secret    = var.kafka_api_secret
- # kafka_id              = confluent_kafka_cluster.dedicated2.id
- # kafka_rest_endpoint   = confluent_kafka_cluster.dedicated2.rest_endpoint
- # kafka_api_key         = confluent_api_key.app-manager-kafka-api-key2.id
- # kafka_api_secret      = confluent_api_key.app-manager-kafka-api-key2.secret
- # cloud_api_key         = var.confluent_cloud_api_key
- # cloud_api_secret      = var.confluent_cloud_api_secret
- # environment_id        = confluent_environment.staging.id
- # kafka_api_version     = confluent_kafka_cluster.dedicated2.api_version
- # kafka_kind            = confluent_kafka_cluster.dedicated2.kind
- # confluent_service_account_id          = confluent_service_account.app-manager2.id
- # confluent_service_account_api_version = confluent_service_account.app-manager2.api_version
- # confluent_service_account_kind        = confluent_service_account.app-manager2.kind
- # topics                = jsondecode(file("topics2.json"))
+  kafka_id              = confluent_kafka_cluster.dedicated2.id
+  kafka_rest_endpoint   = confluent_kafka_cluster.dedicated2.rest_endpoint
+  kafka_api_key         = confluent_api_key.app-manager-kafka-api-key2.id
+  kafka_api_secret      = confluent_api_key.app-manager-kafka-api-key2.secret
+  cloud_api_key         = var.confluent_cloud_api_key
+  cloud_api_secret      = var.confluent_cloud_api_secret
+  environment_id        = confluent_environment.staging.id
+  kafka_api_version     = confluent_kafka_cluster.dedicated2.api_version
+  kafka_kind            = confluent_kafka_cluster.dedicated2.kind
+  confluent_service_account_id          = confluent_service_account.app-manager2.id
+  confluent_service_account_api_version = confluent_service_account.app-manager2.api_version
+  confluent_service_account_kind        = confluent_service_account.app-manager2.kind
+  topics                = jsondecode(file("topics2.json"))
 
-  #depends_on = [
-  #  confluent_kafka_cluster.dedicated2
-  #]
+  depends_on = [
+    confluent_kafka_cluster.dedicated2
+    confluent_environment.staging,
+    confluent_service_account.app-manager2,
+    confluent_api_key.app-manager-kafka-api-key2,
+    confluent_network.peering,
+    confluent_peering.aws,
+    confluent_role_binding.app-manager-kafka-cluster-admin2,
+    aws_vpc_peering_connection_accepter.peer,
+    aws_route.r  
+  ]
   
-#}
+}
 
 resource "confluent_network" "peering" {
   display_name     = "Peering Network"
@@ -217,21 +235,21 @@ resource "confluent_kafka_cluster" "dedicated" {
   ]
 }
 
-#resource "confluent_kafka_cluster" "dedicated2" {
-#  display_name = "inventory2"
-#  availability = "SINGLE_ZONE"
-#  cloud        = confluent_network.peering.cloud
-#  region       = confluent_network.peering.region
-#  dedicated {
-#    cku = 1
-#  }
-#  environment {
-#    id = confluent_environment.staging.id
-#  }
-#  network {
-#    id = confluent_network.peering.id
-#  }
-#}
+resource "confluent_kafka_cluster" "dedicated2" {
+  display_name = "inventory2"
+  availability = "SINGLE_ZONE"
+  cloud        = confluent_network.peering.cloud
+  region       = confluent_network.peering.region
+  dedicated {
+    cku = 1
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  network {
+    id = confluent_network.peering.id
+  }
+}
 
 
 
